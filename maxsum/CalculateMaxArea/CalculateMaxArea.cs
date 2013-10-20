80205c29818941d3f83c8eb64ba2ce9a7075db10
Junpeng Xiao <tonyshaw@yeah.net>
2013-10-20 22:02:25 +0800

    BasicFrame

--- maxsum/CalculateMaxArea/CalculateMaxArea.cs
+++ maxsum/CalculateMaxArea/CalculateMaxArea.cs
@@ -11,27 +11,75 @@ namespace CalculateMaxArea
     {
         int[,] table;
         bool[,] select;
-        int n,m;
+        int n, m;
+        bool isHorizontal;
         public ProcessCore(string filename)
         {
             string[] strs = File.ReadAllLines(@filename);
             if (strs.Length < 2) Console.WriteLine("文件格式不合法");
             string[] subStrs = strs[0].Split(',');
+            isHorizontal = false;
             n = int.Parse(subStrs[0]);
             subStrs = strs[1].Split(',');
             m = int.Parse(subStrs[0]);
-            table = new int[n,m];
-            select = new bool[n,m];
+            table = new int[n, m];
+            select = new bool[n, m];
             for (int i = 2; i != 2 + n; ++i)
             {
                 subStrs = strs[i].Split(',');
                 for (int j = 0; j != m; ++j)
                     table[i - 2, j] = int.Parse(subStrs[j]);
             }
+            int tmp;
+            if (n > m)
+            {
+                for (int i = 0; i != n; ++i)
+                    for (int j = 0; j != n; ++j)
+                    {
+                        tmp = table[i, j];
+                        table[i, j] = table[j, i];
+                        table[j, i] = tmp;
+                    }
+                tmp = n;
+                n = m;
+                m = tmp;
+                isHorizontal = true;
+            }
         }
+        private void Regular();
+        private void Ring();
+        private void Horizontal();
+        private void Vertical();
+        private void InRegular();
         public bool Calcute(string[] mode)
         {
-
+            switch (mode.Length)
+            {
+                case 0:
+                    Regular();
+                    break;
+                case 1:
+                    switch (mode[0][2])
+                    {
+                        case 'a': InRegular(); break;
+                        case 'h': isHorizontal = !isHorizontal; if (isHorizontal) Horizontal(); else Vertical(); break;
+                        case 'v': if (isHorizontal) Horizontal(); else Vertical(); break;
+                        default:
+                            return false;
+                    }
+                    break;
+                case 2:
+                    Ring();
+                    break;
+                case 3:
+                    InRegular();
+                    break;
+                default:
+                    return false;
+            }
+            return true;
+        }
 
     }
 }
+
